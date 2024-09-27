b8b429a132b1218ab9083d430fb734c930b92895
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-09-27 11:44:15 +0530

    reducerhook

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: state.count = 0 };
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h1>Reducer Hook</h1>
+      <h2>{state.count}</h2>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
+  );
+};
+
+export default App;
